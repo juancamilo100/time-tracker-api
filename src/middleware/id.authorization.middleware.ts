bf1a6993d5dd5326fcadc6b4d0af7eeaf50ee9cc
JuanEspinosa <juancamilo100@hotmail.com>
2019-11-19 13:26:59 -0500

    Formatting

--- src/middleware/id.authorization.middleware.ts
+++ src/middleware/id.authorization.middleware.ts
@@ -4,7 +4,8 @@ import { EmployeeRole } from "../database/entities/employee.entity";
 
 const authorizeEmployeeById: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     if (req.employeeId! !== Number.parseInt(req.params.id, 10) &&
-        req.role !== EmployeeRole.ADMIN) { return next(createError(401, "Unauthorized")); }
+        req.role !== EmployeeRole.ADMIN) 
+    { return next(createError(401, "Unauthorized")); }
 
     next();
 };
