3306ddecf526b573820514a228469432943e2ed7
JuanEspinosa <juancamilo100@hotmail.com>
2019-12-02 21:40:53 -0500

    Added base64 decoding to password fields

--- src/middleware/body.transform.lowercase.middleware.ts
+++ src/middleware/body.transform.lowercase.middleware.ts
@@ -2,13 +2,13 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { ObjectLiteral } from "../../types/generics";
 import { toLowerCaseAllPropsValues } from "../utils/formatter";
 
-const fieldsToExclude = [
-    "password",
-    "oldPassword",
-    "newPassword"
-];
-
 const transformBodyPropsValuesToLowerCase: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const fieldsToExclude = [
+        "password",
+        "oldPassword",
+        "newPassword"
+    ];
+
     req.body = toLowerCaseAllPropsValues(req.body as ObjectLiteral, fieldsToExclude);
     next();
 };
